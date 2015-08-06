09eac6975356bcd3ab01bcc83e26d349507c5a6b
Sean Vivier <seanvivier@hotmail.com>
2015-08-06 09:14:41 -0400

    Debugged

--- Lore.js
+++ Lore.js
@@ -1,28 +1,8 @@
-//VARIABLES
-
-//Special Spanish characters
-var Aacute = "\xC1";
-var aacute = "\xE1";
-var Eacute = "\xC9";
-var eacute = "\xE9";
-var Iacute = "\xCD";
-var iacute = "\xED";
-var Oacute = "\xD3";
-var oacute = "\xF3";
-var Uacute = "\xDA";
-var uacute = "\xFA";
-var Ntilde = "\xD1";
-var ntilde = "\xF1";
-var Udieresis = "\xDC"
-var udieresis = "\xFC"
-var invertedExclamation = "\uA1";
-var invertedInterrogation = "\xBF";
-
 //FUNCTIONS
 
 //Choose a random number between two numbers.  Inspired by Python's randint.
 function chance(x, y) {
-	return Math.round(Math.random()*(y-x) + x)
+	return Math.round(Math.random()*(y-x) + x);
 }
 
 //A function to randomly choose a percentage probability
@@ -32,7 +12,7 @@ function prob() {
 
 //Find the value of n to a certain number of significant figures.
 function sigFig(n, place) {
-	return Math.floor(n*place)/place;
+	return Math.floor((n*place)/place);
 }
 
 //a function for AJAX.
