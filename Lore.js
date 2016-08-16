caa97c861cfb26bc9c1cd5c758e5ba5b44b3d176
Sean Vivier <seanvivier@hotmail.com>
2016-08-15 21:32:11 -0400

    Additions
    Added functionality to give all headers the same class, so they can all
    be targeted at once.  Added variables for common Spanish diacritics.
    Added cap(), majuscule(), .holds(), perseverate(), .drop(), and
    wordCount() functions.

--- Lore.js
+++ Lore.js
@@ -1,3 +1,31 @@
+//So that all headings can be targeted at once.
+$(document).ready(function() {
+    $("h1").addClass("h");
+    $("h2").addClass("h");
+    $("h3").addClass("h");
+    $("h4").addClass("h");
+    $("h5").addClass("h");
+    $("h6").addClass("h");
+});
+
+//VARIABLES
+
+//Spanish special characters
+var AAcute = "\xC1";
+var aAcute = "\xE1";
+var EAcute = "\xC9";
+var eAcute = "\xE9";
+var IAcute = "\xCD";
+var iAcute = "\xED";
+var OAcute = "\xD3";
+var oAcute = "\xF3";
+var UAcute = "\xDA";
+var uAcute = "\xFA";
+var UUml = "\xDC";
+var uUml = "\xFC";
+var iQuest = "\xBF";
+var iExcl = "\xA1";
+
 //FUNCTIONS
 
 //Choose a random number between two numbers.  Inspired by Python's randint.
@@ -48,4 +76,59 @@ function grab(param) {
 		grabs[pair[0]] = pair[1];
 	}
 	return decodeURIComponent(grabs[param]);
+}
+
+//a function to capitalize the first letter of a word or string
+function cap(string) {
+	var arr = string.split("");
+	arr[0] = arr[0].toUpperCase();
+	return arr.join("");
+}
+
+//a function to capitalize the first letter of every word
+function majuscule(string) {
+	var arr = string.split(" ");
+	for (i=0; i<arr.length; i++) {
+		arr[i] = cap(arr[i]);
+	}
+	return arr.join(" ");
+}
+
+//a function for a simple Boolean response to contained text
+String.prototype.holds = function(something) {
+	if (this.indexOf(something) > -1) {
+		return true;
+	} else {
+		return false;
+	}
+}
+Array.prototype.holds = function(something) {
+	if (this.indexOf(something) > -1) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+//a function to set an interval that activates immediately
+function perseverate(func, time) {
+	func();
+	setInterval(function() {
+		func();
+	}, time);
+}
+
+//drop an item from an array
+Array.prototype.drop = function(thing) {
+	for (i=this.length-1; i>=0; i--) {
+		if (this[i] === thing) {
+			this.splice(i, 1);
+		}
+	}
+}
+
+//a simple word count function
+function wordCount(txt) {
+        var sacrificialString = txt;
+        return sacrificialString.split(/\s+/).length;
 }
